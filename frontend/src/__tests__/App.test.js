f7e6361d43878dfa49f9aa4bcf8e9b6e5a91135e
Alexander Budko <103473998+alex-budko@users.noreply.github.com>
2023-05-03 04:42:13 -0400

    workflow testing

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,18 +1,46 @@
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-describe("App component", () => {
-  test("renders login and register buttons if not logged in", () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    const loginButton = screen.getByText("Login");
-    const registerButton = screen.getByText("Register");
+test("renders Login and Register buttons", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 
-    expect(loginButton).toBeInTheDocument();
-    expect(registerButton).toBeInTheDocument();
-  });
+  const loginButton = screen.getByText(/Login/i);
+  expect(loginButton).toBeInTheDocument();
+
+  const registerButton = screen.getByText(/Register/i);
+  expect(registerButton).toBeInTheDocument();
+});
+
+test("navigates to Login page", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByText(/Login/i);
+  userEvent.click(loginButton);
+
+  const loginForm = screen.getByText(/Sign in to your account/i);
+  expect(loginForm).toBeInTheDocument();
+});
+
+test("navigates to Register page", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  const registerButton = screen.getByText(/Register/i);
+  userEvent.click(registerButton);
+
+  const registerForm = screen.getByText(/Create a new account/i);
+  expect(registerForm).toBeInTheDocument();
 });
--- server/__tests__/server.test.js
+++ server/__tests__/server.test.js
@@ -1,9 +1,37 @@
 const request = require("supertest");
 const app = require("../server");
+const User = require("../models/User");
 
-describe("API Endpoints", () => {
-  test("GET /messages should return 401 if not logged in", async () => {
-    const response = await request(app).get("/messages");
-    expect(response.statusCode).toBe(401);
+const testUser = {
+  username: "testuser",
+  password: "testpassword",
+};
+
+beforeAll(async () => {
+  await User.deleteMany({});
+});
+
+afterAll(async () => {
+  await User.deleteMany({});
+});
+
+test("Register a new user", async () => {
+  const res = await request(app).post("/register").send(testUser);
+  expect(res.statusCode).toBe(201);
+  expect(res.body).toHaveProperty("token");
+});
+
+test("Login with registered user", async () => {
+  const res = await request(app).post("/login").send(testUser);
+  expect(res.statusCode).toBe(200);
+  expect(res.body).toHaveProperty("token");
+  expect(res.body).toHaveProperty("user");
+});
+
+test("Fail to login with wrong credentials", async () => {
+  const res = await request(app).post("/login").send({
+    username: "wrongusername",
+    password: "wrongpassword",
   });
+  expect(res.statusCode).toBe(401);
 });
