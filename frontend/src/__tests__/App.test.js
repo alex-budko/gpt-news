62b60efc7848c8a425fda993d4dcf3bf4d3e9c58
Alexander Budko <103473998+alex-budko@users.noreply.github.com>
2023-05-03 04:47:40 -0400

    changes

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from "./App";
+import App from "../App";
 import { BrowserRouter } from "react-router-dom";
 
 test("renders Login and Register buttons", () => {
